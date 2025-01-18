87bcb54d347a534f286bec88777c77d9204bee1c
91charlie <cgi1004a@gmail.com>
2025-01-18 18:37:05 +0900

    second commit

---
+++ src/Main.kt
@@ -0,0 +1,20 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+
+fun cm(a:Int)
+{
+    println(a*2.54)
+}
+fun inch(a:Int)
+{
+    println(a/2.54)
+}
+
+
+fun main()
+{
+    val a:Int = 28
+    val b:Int = 71
+    cm(a)
+    inch(b)
+}
